761a122c34b8ace09e0804ada5666cc468fbc732
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-11-20 16:35:34 +0100

    En cours.

---
+++ TransportsEnCommun_001/MetroRequests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportsEnCommun_001
+{
+    public class MetroRequests
+    {
+        public string GetLinesNear()
+        {
+            string retour = LinesNear();
+            return retour;
+        }
+
+        public string GetLinesNearProx(double latitude = 5.727718, double longitude = 45.185603, float distance = 500, bool details = false)
+        {
+            string retour = LinesNearProx(latitude, longitude, distance, details);
+            return retour;
+        }
+
+        private string LinesNear()
+        {
+
+            WebRequest request = WebRequest.Create("https://data.metromobilite.fr/api/routers/default/index/routes");
+
+            HttpWebResponse response2 = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(response2.StatusDescription);
+
+            Stream dataStream = response2.GetResponseStream();
+
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            //Console.WriteLine(responseFromServer);
+            //WebResponse response2 = request.GetResponse();
+            //Stream dataStream = response2.GetResponseStream();
+            //StreamReader reader = new StreamReader(dataStream);
+
+            reader.Close();
+            dataStream.Close();
+            response2.Close();
+
+            return responseFromServer;
+        }
+
+        private string LinesNearProx(double x, double y, float z, bool details)
+        {
+
+            WebRequest request = WebRequest.Create($"http://data.metromobilite.fr/api/linesNear/json?x={x}&y={y}&dist={z}&details={details}");
+
+            HttpWebResponse response2 = (HttpWebResponse)request.GetResponse();
+
+            Console.WriteLine(response2.StatusDescription);
+
+            Stream dataStream = response2.GetResponseStream();
+
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            //Console.WriteLine(responseFromServer);
+            //WebResponse response2 = request.GetResponse();
+            //Stream dataStream = response2.GetResponseStream();
+            //StreamReader reader = new StreamReader(dataStream);
+
+            reader.Close();
+            dataStream.Close();
+            response2.Close();
+
+            return responseFromServer;
+        }
+
+    }
+}
--- TransportsEnCommun_001/Program.cs
+++ TransportsEnCommun_001/Program.cs
@@ -1,17 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TransportsEnCommun_001
 {
+    public class LinesNear
+    {
+        public string id { get; set; }
+        public string gtfsId { get; set; }
+        public string shortName { get; set; }
+        public string longName { get; set; }
+        public string color { get; set; }
+        public string textColor { get; set; }
+        public string mode { get; set; }
+        public string type { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            string listMetro;
+            string ListMetroProx;
+            MetroRequests metrorequests = new MetroRequests();
+                    listMetro = metrorequests.GetLinesNear();
+                    Console.WriteLine(listMetro);
+
+            Console.WriteLine("**************************************************************************************");
+
+            ListMetroProx = metrorequests.GetLinesNearProx(5.727718, 45.185603, 500, true);
+                    Console.WriteLine(ListMetroProx);
+            //WebRequest request = WebRequest.Create("https://data.metromobilite.fr/api/routers/default/index/routes");
+
+            //HttpWebResponse response2 = (HttpWebResponse)request.GetResponse();
+
+            //Console.WriteLine(response2.StatusDescription);
+
+            //Stream dataStream = response2.GetResponseStream();
+
+            //StreamReader reader = new StreamReader(dataStream);
+
+            //string responseFromServer = reader.ReadToEnd();
+
+            //Console.WriteLine(responseFromServer);
+
+            //WebResponse response2 = request.GetResponse();
+            //Stream dataStream = response2.GetResponseStream();
+            //StreamReader reader = new StreamReader(dataStream);
+
+            //reader.Close();
+            //dataStream.Close();
+            //response2.Close();
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
         }
+    
     }
 }
