0b7583869bbb8ec857139b7fa06de440189867bd
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-11-20 13:36:58 +0100

    Environement VSCode.

---
+++ TransportsEnCommun_001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportsEnCommun_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+        }
+    }
+}
