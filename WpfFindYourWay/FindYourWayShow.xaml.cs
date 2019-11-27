eeca9ff5eb2d15a801e7b6b358e9c7a566fcee46
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-11-27 13:52:27 +0100

    Add WPF V2 Binding Items Dictionary

--- WpfFindYourWay/FindYourWayShow.xaml.cs
+++ WpfFindYourWay/FindYourWayShow.xaml.cs
@@ -23,42 +23,50 @@ namespace WpfFindYourWay
     /// </summary>
     public partial class FindYourWayShow : Page
     {
-        public string Stations
+        public Dictionary<string, SerializeProxima> Stations
         {
-            get { return (string)GetValue(StationsProperty); }
+            get { return (Dictionary<string, SerializeProxima>)GetValue(StationsProperty); }
             set { SetValue(StationsProperty, value); }
         }
 
-        public static readonly DependencyProperty StationsProperty = DependencyProperty.Register("Stations", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty StationsProperty = DependencyProperty.Register("Stations", typeof(Dictionary<string, SerializeProxima>), typeof(MainWindow));
+
         public FindYourWayShow()
         {
             InitializeComponent();
             this.DataContext = this;
-            this.Stations = "Test!!!";
+            //this.Stations = "Test!!!";
             //TextBlock textBlock = new TextBlock();
             //textBlock.Text = "Ceci est un TEST!!!";
 
             Metro linemetro = new Metro();
 
-            Dictionary<string, SerializeProxima> LineMetro;
+            //Dictionary<string, SerializeProxima> LineMetro;
 
-            LineMetro = linemetro.GetLinesMetro("5.727718", "45.185603", 500, true, "http://data.metromobilite.fr/api/linesNear/json?x=5.727718&y=45.185603&dist=500&details=true");
+            this.Stations = linemetro.GetLinesMetro("5.727718", "45.185603", 500, true, "http://data.metromobilite.fr/api/linesNear/json?x=5.727718&y=45.185603&dist=500&details=true");
 
-            IDictionaryEnumerator myEnumerator1 = LineMetro.GetEnumerator();
+            //IDictionaryEnumerator myEnumerator1 = LineMetro.GetEnumerator();
+
+            //this.Stations = LineMetro
+
+            //this.Stations = LineMetro["GRENOBLE, CASERNE DE BONNE"].Lines[0].ToString();
+            //this.Stations = LineMetro["GRENOBLE, CASERNE DE BONNE"].Zone.ToString();
+            //this.Stations = LineMetro["GRENOBLE, CASERNE DE BONNE"].Name.ToString();
+
 
             // If MoveNext passes the end of the 
             // collection, the enumerator is positioned 
             // after the last element in the collection 
             // and MoveNext returns false. 
-            while (myEnumerator1.MoveNext())
-            {
-                string json1 = JsonConvert.SerializeObject(myEnumerator1.Value, Formatting.Indented, new JsonSerializerSettings
-                {
-                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-                });
-                //Console.WriteLine(myEnumerator1.Key + " --> " + json1);
-                this.Stations = (myEnumerator1.Key + " --> " + json1);
-            }
+            //while (myEnumerator1.MoveNext())
+            //{
+            //    string json1 = JsonConvert.SerializeObject(myEnumerator1.Value, Formatting.Indented, new JsonSerializerSettings
+            //    {
+            //        ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            //    });
+            //    //Console.WriteLine(myEnumerator1.Key + " --> " + json1);
+            //    this.Stations = myEnumerator1.Key + " --> " + json1;
+            //}
         }
     }
 }
